5958b89367def9ec7843d73b449ca85c3253e1f1
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-29 17:27:00 +0000

    added script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
  * Created on: Mar 2022
  * This file contains the JS functions for index.html 
 */
+
+'use strict'
+
+/**
+ * This function calculates the area and perimeter of the rectangle.
+ */
+function calculate () {
+  // input
+  const baseA = parseInt(document.getElementById('base-of-triangle').value)
+  const baseB = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'The Area of the triangle is: ' + area + ' cm²'
+}
